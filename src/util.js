5d763954070741063d1e56dcee5b338d17f1fcbd
slurmulon <anon@blaqkmail.com>
2015-10-07 14:06:32 -0700

    :boom: moved url regex out of core module

--- src/util.js
+++ src/util.js
@@ -9,8 +9,3 @@ export const is = (x, y) => x === y || Object.is(x, y)
  * Safely merges together N objects (leaves source objects untouched)
  */
 export const mix = () => Object.assign({}, ...arguments)
-
-/**
- * Regex matching URLs
- */
-export const url = /(http(?:s)?\:\/\/[a-zA-Z0-9]+(?:(?:\.|\-)[a-zA-Z0-9]+)+(?:\:\d+)?(?:\/[\w\-]+)*(?:\/?|\/\w+\.[a-zA-Z]{2,4}(?:\?[\w]+\=[\w\-]+)?)?(?:\&[\w]+\=[\w\-]+)*)/g
