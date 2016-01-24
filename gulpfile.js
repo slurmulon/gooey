3bd49e2e07fdc785ed20880dca2f3500cdf7aab3
slurmulon <me@madhax.io>
2016-01-23 17:39:29 -0800

    :wrench: uglification (also integrated into )

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,38 @@
 var gulp       = require('gulp'),
     babel      = require('gulp-babel'),
     shell      = require('gulp-shell'),
+    uglify     = require('gulp-uglify'),
     source     = require('vinyl-source-stream'),
     browserify = require('browserify')
 
 gulp.task('default', ['compile'])
 
-gulp.task('clean', shell.task(['rm -rf dist']))
+gulp.task('clean', shell.task(['rm -rf lib && rm -rf dist']))
 
 gulp.task('compile', ['clean'], function() {
   return gulp
     .src('*.js', {cwd: 'src', read: true})
     .pipe(babel())
+    .pipe(gulp.dest('lib'))
+})
+
+gulp.task('compress', ['compile'], function() {
+  return gulp
+    .src('./lib/*.js')
+    .pipe(uglify())
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('browserify', ['compile'], function() {
-  return browserify('./dist/index.js')
+gulp.task('browserify', ['compress'], function() {
+  browserify('./dist/index.js')
     .bundle()
-    .pipe(source('gooey-browser.js'))
-    .pipe(gulp.dest('./browser/'))
+    .pipe(source('gooey-core.js'))
+    .pipe(gulp.dest('browser'))
+
+  return gulp
+    .src('./browser/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('browser'))
 })
 
 gulp.task('test', ['compile'], shell.task(['node ./node_modules/.bin/_mocha --reporter nyan --compilers js:babel/register test/']))
